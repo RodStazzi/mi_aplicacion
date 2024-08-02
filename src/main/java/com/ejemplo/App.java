a6cc2253d21d6c34adc091a044315993a06ae341
RodStazzi <rodolfostazzi@gmail.com>
2024-08-01 23:09:58 -0400

    Modificaciones con pruebas de testing

--- src/main/java/com/ejemplo/App.java
+++ src/main/java/com/ejemplo/App.java
@@ -1,5 +1,7 @@
 package com.ejemplo;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Function;
 
 
@@ -10,16 +12,25 @@ import java.util.function.Function;
 public class App 
 {
     public static void main( String[] args )
-    {
-        System.out.println( "El nuevo 2.1.0 Group" );
+    {	System.out.println( "========================" );
+        System.out.println( "||El nuevo 2.1.0 Group||" );
+        System.out.println( "========================" );
 
         Function<Integer, Integer> square = x -> x * x;
-
+        Function<Integer, Integer> squareCube = x -> x * x * x;
         // Ejemplo de uso
         int num = 5;
         int result = square.apply(num);
-        System.out.println("El cuadrado de " + num + " es: " + result);
-
+        int resultCube = squareCube.apply(num);
+        
+        List<Integer> numeros = Arrays.asList(2, 5, 6, 8, 18, 34);
+        
+        for (int i = 0; i < numeros.size(); i++) {
+        	System.out.println("Número a operar "+numeros.get(i));
+            System.out.println("El cuadrado de " + num + " es: " + square.apply(numeros.get(i))+" y su cubo es: " + squareCube.apply(numeros.get(i)));
+            System.out.println("___________________________________________");
+        	
+		}
     }
 }
 
--- src/test/java/com/ejemplo/AppTest.java
+++ src/test/java/com/ejemplo/AppTest.java
@@ -1,57 +1,56 @@
 package com.ejemplo;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-// import org.testng.annotations.Test;
-// import java.util.function.Function;
-// import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest {
+
+    @Test
+    public void testSquareFunction() {
+        // Definir la función square
+        Function<Integer, Integer> square = x -> x * x;
+
+        // Pruebas unitarias para la función square
+        assertEquals(4, (int) square.apply(2));
+        assertEquals(25, (int) square.apply(5));
+        assertEquals(36, (int) square.apply(6));
+        assertEquals(64, (int) square.apply(8));
+        assertEquals(324, (int) square.apply(18));
+        assertEquals(1156, (int) square.apply(34));
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    public void testSquareCubeFunction() {
+        // Definir la función squareCube
+        Function<Integer, Integer> squareCube = x -> x * x * x;
+
+        // Pruebas unitarias para la función squareCube
+        assertEquals(8, (int) squareCube.apply(2));
+        assertEquals(125, (int) squareCube.apply(5));
+        assertEquals(216, (int) squareCube.apply(6));
+        assertEquals(512, (int) squareCube.apply(8));
+        assertEquals(5832, (int) squareCube.apply(18));
+        assertEquals(39304, (int) squareCube.apply(34));
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void testSquareAndSquareCubeWithList() {
+        // Definir las funciones
+        Function<Integer, Integer> square = x -> x * x;
+        Function<Integer, Integer> squareCube = x -> x * x * x;
+
+        // Lista de números de prueba
+        List<Integer> numeros = Arrays.asList(2, 5, 6, 8, 18, 34);
+
+        // Validar los resultados de aplicar las funciones a la lista
+        assertEquals(4, (int) square.apply(numeros.get(0)));
+        assertEquals(125, (int) squareCube.apply(numeros.get(1)));
+        assertEquals(36, (int) square.apply(numeros.get(2)));
+        assertEquals(512, (int) squareCube.apply(numeros.get(3)));
+        assertEquals(324, (int) square.apply(numeros.get(4)));
+        assertEquals(39304, (int) squareCube.apply(numeros.get(5)));
     }
 }
-
-
-// public class AppTest {
-
-//     @Test
-//     public void testSquareFunction() {
-//         // Arrange
-//         Function<Integer, Integer> square = x -> x * x;
-//         int num = 5;
-
-//         // Act
-//         int result = square.apply(num);
-
-//         // Assert
-//         assertEquals(result, 25, "El cuadrado de 5 debe ser 25");
-//     }
-// }
